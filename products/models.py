64ae248236bc44b4bf8a4504a5cc973aed00af5f
ditosepha <osephashvili.dito@gmail.com>
2024-06-27 16:05:04 +0400

    add basic custom permission for review api

--- products/models.py
+++ products/models.py
@@ -41,7 +41,7 @@ class Review(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     review_text = models.TextField()
     rating = models.DecimalField(max_digits=2, decimal_places=1)
-    data_added = models.DateTimeField(default=timezone.now)
+    date_added = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return f'{self.user.username} - {self.product.name}'
--- products/permissions.py
+++ products/permissions.py
@@ -17,3 +17,22 @@ class IsCompanyOrReadOnly(BasePermission):
                 return True
             else:
                 return False
+            
+class IsCustomerOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        elif request.method not in SAFE_METHODS and request.user.role == "customer":
+            return True
+        else:
+            return False
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS or request.user.role == "admin":
+            return True
+        elif request.user.role == "customer":
+            if obj.user == request.user.pk:
+                return True
+            else: 
+                return False
+            
